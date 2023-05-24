d71a4a1894879e7a75ff23cee0ed63ae19a2b78e
Daomtthuan <dao.mt.thuan@gmail.com>
2023-05-25 03:46:42 +0700

    Update

--- image-filter-starter-code/src/util/util.ts
+++ image-filter-starter-code/src/util/util.ts
@@ -14,15 +14,14 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
       const res = await fetch(inputURL);
       const buffer = await res.arrayBuffer();
       const photo = await Jimp.read(buffer as Buffer);
+      const outPath = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
 
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
-          resolve(__dirname + outpath);
+        .write(__dirname + outPath, (img) => {
+          resolve(__dirname + outPath);
         });
     } catch (error) {
       reject(error);
